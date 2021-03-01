2e40da02b30eec2f19c9c734b21ae43717585a39
xutao <tao4.xu@zhangmen.com>
2021-03-01 20:47:21 +0800

    recode

--- app/src/main/java/com/example/androiddevchallenge/DetailsActivity.kt
+++ app/src/main/java/com/example/androiddevchallenge/DetailsActivity.kt
@@ -1,28 +1,14 @@
-/*
- * Copyright 2021 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package com.example.androiddevchallenge
 
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
@@ -36,7 +22,6 @@ import androidx.compose.ui.res.imageResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.androiddevchallenge.ui.MainList
 import com.example.androiddevchallenge.ui.data.Animal
 import com.example.androiddevchallenge.ui.theme.MyTheme
 
@@ -72,7 +57,8 @@ class DetailsActivity : AppCompatActivity() {
                 Column(
                     Modifier
                         .fillMaxHeight()
-                        .padding(8.dp)) {
+                        .padding(8.dp)
+                ) {
                     Text(text = animal.name, fontSize = 50.sp)
                     Text(text = animal.desc, maxLines = 1, overflow = TextOverflow.Ellipsis)
                 }
