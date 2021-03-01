a0b38628cf4edd37e0db5eac3f87ca321214d9c0
xutao <tao4.xu@zhangmen.com>
2021-03-01 20:45:19 +0800

    recode

--- app/src/main/java/com/example/androiddevchallenge/ui/MainList.kt
+++ app/src/main/java/com/example/androiddevchallenge/ui/MainList.kt
@@ -3,7 +3,12 @@ package com.example.androiddevchallenge.ui
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
